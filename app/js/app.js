9820ca694c861abc3ba928304f7205fc2a22d938
Benoit <benoit.chabord@grabone.co.nz>
2014-06-19 17:55:59 +1200

    try to se dynamic ip

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,22 @@
 var visulastic = angular.module('visulastic', ['ui.bootstrap', 'elasticsearch']);
 
 visulastic.service('es', function (esFactory) {
-  return esFactory({
-    host: 'localhost:9200'
-  });
+    'use strict';
+
+    var host = '10.50.20.208:9200';
+    var esClient = esFactory({ host: host });
+
+    var setHost = function (h){
+        host = h;
+        esClient = esFactory({ host: host });
+    };
+
+
+    var state = {}
+
+    return {
+        getClient: function(){ return esClient; },
+        getHost: function (){ return host; },
+        setHost: setHost
+    };
 });
--- app/js/controller/mycontroller.js
+++ app/js/controller/mycontroller.js
@@ -3,7 +3,7 @@
 visulastic.controller('VisulasticCtrl', ['$scope','es', function($scope, es) {
   $scope.status = "Undifined";
 
-  es.ping({
+  es.getClient().ping({
       requestTimeout: 1000,
       hello: "elasticsearch!"
     }).then(
@@ -15,6 +15,17 @@ visulastic.controller('VisulasticCtrl', ['$scope','es', function($scope, es) {
 
     $scope.cluster = "Undif";
 
-    es.cluster.state().then(function(value){$scope.cluster = value;});
+    es.getClient().cluster.state().then(function(value){$scope.cluster = value;});
 
   }]);
+
+    visulastic.controller('HeaderCtrl', ['$scope','es', function($scope, es) {
+        $scope.host = es.getHost();
+
+        $scope.setServer = function(h){
+            es.setHost(h);
+            console.log(h);
+        }
+
+
+}]);
